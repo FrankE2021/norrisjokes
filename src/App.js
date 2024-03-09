b07f1d893797264e07b63ac8fb3869863760eeab
Soy Frank <ernestinversion@gmail.com>
2024-03-09 18:12:11 -0300

    nuevo1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [chiste, setChiste] = useState("")
+  const otro=()=>{
+    const url= "https://api.chucknorris.io/jokes/random"
+    const peticion=fetch(url);
+    peticion
+    .then(datos => datos.json())
+    .then(lectura => setChiste(lectura.value))
+    .catch(console.log)
+  }
+  useEffect(()=> {
+    otro()
+  },[])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='contenedor'>
+  <h2>
+    {chiste}
+  </h2>  
+  <p>
+    <button className='but' onClick={otro}>Otro chiste</button>
+  </p> 
+  </div> 
+  </>
   );
 }
 
